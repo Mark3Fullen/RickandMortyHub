90286e80a5081fbdff9023c0aadff99fa011e123
Mark <mark3fullen@gmail.com>
2021-07-12 14:46:03 -0400

    Added basic JS - 3

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 document.addEventListener('DOMContentLoaded', () => {
     console.log("Bruh Moment")
+    fetchAPI()
 })
+
+function fetchAPI() {
+    fetch('https://rickandmortyapi.com/api/character/1')
+    .then(res => res.json())
+    .then(json => renderCharacters(json))
+}
+
+function renderCharacters(data) {
+    console.log(data)
+}
