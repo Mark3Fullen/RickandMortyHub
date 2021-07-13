d703d1bf68eaa19f854a0a372dbcf90d500081ef
nhuse <nathan.huse@yahoo.com>
2021-07-13 08:27:04 -0500

    Small Update to JS

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-function parseTrails() {
+document.addEventListener('DOMContentLoaded', () => {
+    parseCharacters();
+})
+
+function parseCharacters() {
     fetch('https://rickandmortyapi.com/api/character')
     .then(response => response.text())
     .then(result => console.log(result))
     .catch(error => console.log('error', error));
 }
-
-parseTrails();
